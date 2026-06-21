120ffe8295b14402567c343d9ce2c38953787a33
moalaa125 <muhamedawadkassem@gmail.com>
2026-06-21 19:03:53 +0300

    implement offline mode

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,6 +6,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:store_app/models/product_model.dart';
 import 'package:store_app/services/get_all_products_services.dart';
 import 'package:store_app/widgets/custom_card.dart';
+import 'package:flutter_offline/flutter_offline.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -23,6 +24,72 @@ class _HomeState extends State<Home> {
   void initState() {
     super.initState();
     futureProducts = AllProductsService().getAllProducts();
+  }
+
+  Widget _buildNoInternet() {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/error1.png'),
+          SizedBox(height: 20.h),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'No internet Connection Pleas Check your internet and try again !',
+              style: TextStyle(fontSize: 25.sp, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );  
+  }
+
+  Widget _buildItems() {
+    return Stack(
+      children: [
+        FutureBuilder<List<ProductModel>>(
+          future: futureProducts,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(color: Colors.black),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('Oops! An error occurred: \n${snapshot.error}'),
+              );
+            } else if (snapshot.hasData) {
+              List<ProductModel> products = snapshot.data!;
+
+              return GridView.builder(
+                padding: EdgeInsets.only(
+                  top: 70.h,
+                  left: 10.w,
+                  right: 10.w,
+                  bottom: 80.h,
+                ),
+                clipBehavior: Clip.none,
+                itemCount: products.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1.4,
+                  crossAxisSpacing: 20.w,
+                  mainAxisSpacing: 100.h,
+                ),
+                itemBuilder: (context, index) {
+                  return CustomCard(product: products[index]);
+                },
+              );
+            } else {
+              return Center(child: Text('No products found.'));
+            }
+          },
+        ),
+      ],
+    );
   }
 
   @override
@@ -54,47 +121,23 @@ class _HomeState extends State<Home> {
         ),
         centerTitle: true,
       ),
-      body: Stack(
-        children: [
-          FutureBuilder<List<ProductModel>>(
-            future: futureProducts,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: CircularProgressIndicator(color: Colors.black),
-                );
-              } else if (snapshot.hasError) {
-                return Center(
-                  child: Text('Oops! An error occurred: \n${snapshot.error}'),
-                );
-              } else if (snapshot.hasData) {
-                List<ProductModel> products = snapshot.data!;
-
-                return GridView.builder(
-                  padding: EdgeInsets.only(
-                    top: 70.h,
-                    left: 10.w,
-                    right: 10.w,
-                    bottom: 80.h,
-                  ),
-                  clipBehavior: Clip.none,
-                  itemCount: products.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 1.4,
-                    crossAxisSpacing: 20.w,
-                    mainAxisSpacing: 100.h,
-                  ),
-                  itemBuilder: (context, index) {
-                    return CustomCard(product: products[index]);
-                  },
-                );
+      body: OfflineBuilder(
+        connectivityBuilder:
+            (
+              BuildContext context,
+              List<ConnectivityResult> connectivity,
+              Widget child,
+            ) {
+              final bool connected = !connectivity.contains(
+                ConnectivityResult.none,
+              );
+              if (connected) {
+                return _buildItems();
               } else {
-                return Center(child: Text('No products found.'));
+                return _buildNoInternet();
               }
             },
-          ),
-        ],
+        child: CircularProgressIndicator(),
       ),
     );
   }
